de4ec12bc70aa1fbbd4b3cd5444562beb8042c70
Dave-Sama <davech1228@gmail.com>
2021-04-18 20:57:49 +0300

    lab5

--- TCP/client.py
+++ TCP/client.py
@@ -1,8 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-sock.connect(('127.0.0.1', 9999))  # server ip, port
-print('successful connection')
+ print('successful connection')
 while True:
     data = input('Enter line:').strip().encode()
     sock.send(data)
--- TCP/server.py
+++ TCP/server.py
@@ -1,10 +1,44 @@
 import socket
+import threading
 
+# step 1
+ports = [1, 2, 3, 4, 5]
+
+# step 2
+index = int(input('(*) choose index 0-4: '))
+
+# step 3
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-sock.bind(('0.0.0.0', 9999))  # listen port
+# sock.bind(('0.0.0.0', ports[index]))
+sock.bind(('127.0.0.1', ports[index]))
 sock.listen(1)
-conn, client_address = sock.accept()
-print('new connection from', client_address)
-while True:
+
+
+def respond_to_client(conn_socket, client_address):
+    print('start listening from', client_address)
+
     data = conn.recv(1024)
+    if client_address[0] == '127.0.0.1':
+        conn.send(b'Echo: ' + ' world'.encode())
+
+    print('received from', client_address, 'text', data.decode())
     conn.send(b'Echo: ' + data)
+
+
+while True:
+    try:
+        for i in range(len(ports)):
+            if i != index:
+                sock.connect(('127.0.0.1', ports[i]))  # server ip, port
+                print('Connected Successfully!')
+                sock.send('hello '.encode())
+                reply_data = sock.recv(1024)
+                print('server reply:', reply_data.decode())
+
+    except ConnectionRefusedError:
+        print('Connection refused Error!')
+
+    # stuck here!!
+    conn, client_address = sock.accept()
+    print(f'new connection from {client_address}')
+    threading.Thread(target=respond_to_client, args=(conn, client_address)).start()
